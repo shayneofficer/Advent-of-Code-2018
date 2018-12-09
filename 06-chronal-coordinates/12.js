266db20f0bf36585ccc05d38512f1ebcd895ca74
shayneofficer <shayneofficer@gmail.com>
2018-12-09 01:46:36 -0600

    challenge 12 solution

--- 06-chronal-coordinates/12.js
+++ 06-chronal-coordinates/12.js
@@ -29,34 +29,21 @@ fs.readFile("./input.txt", "utf8", (err, data) => {
         coordinateGrid[inputArr[i][0]][inputArr[i][1]] = coordinateNames[i];
     }
 
+    var safeSquares = 0;
     for (var i = 0; i < coordinateGrid.length; i++) {
         for (var j = 0; j < coordinateGrid[i].length; j++) {
-            coordinateGrid[i][j] = findClosestLoc(i, j, inputArr, coordinateGrid);
+            if (isSafe(i, j, inputArr)) {
+                safeSquares++;
+                console.log(`Safe spot: (${i},${j})`);
+                coordinateGrid[i][j] = '#';
+            }
         }
     }
+    console.log(`${safeSquares} safe coordinates`);
+    // Solution: 43852 safe coordinates
 
     // printGrid(coordinateGrid);
 
-    var mapAreas = [];
-    for (var i = 0; i < 50; i++) {
-        mapAreas.push(0);
-    }
-
-    for (var i = 0; i < coordinateGrid.length; i++) {
-        for (var j = 0; j < coordinateGrid[i].length; j++) {
-            var letterAtLocation = coordinateGrid[i][j];
-            mapAreas[coordinateNames.indexOf(letterAtLocation)]++;
-        }
-    }
-
-    // Get the number of coordinates occupied by each letter
-    for (var i = 0; i < mapAreas.length; i++) {
-        console.log(`${coordinateNames[i]}: ${mapAreas[i]}`);
-    }
-
-    // From here I just looked at the print out in grid.txt and eliminated letters in contact with the edge of the map (infinite)
-    // The largest remaining value was "V" with 3882 locations.
-    // This felt a like a little bit of a cheesy solution, so if I have time I'd like to look for a better one.
 })
 
 // Function to print out the coordinate grid to the grid.txt file (commented out call s.t. it doesn't keep appending)
@@ -66,7 +53,7 @@ function printGrid(arr) {
         for (var i = 0; i < arr.length; i++) {
             rowStr += arr[i][j];
         }
-        fs.appendFile("./grid.txt", rowStr + "\n", (err) => {
+        fs.appendFile("./grid_safe.txt", rowStr + "\n", (err) => {
             if (err) {
                 console.log(err);
             }
@@ -74,22 +61,12 @@ function printGrid(arr) {
     }
 }
 
-// Function that takes an x and y coordinate, then finds the location in the input the shortest distance away
-function findClosestLoc(x, y, arr, grid) {
-    var shortestPath = Math.abs(arr[0][0] - x) + Math.abs(arr[0][1] - y);
-    closestPoint = "";
-
+// Function that takes an x and y coordinate, then determines whether the total distance to each input is less than 10,000
+function isSafe(x, y, arr) {
+    var totalDist = 0;
     for (var i = 0; i < arr.length; i++) {
-        var dist = Math.abs(arr[i][0] - x) + Math.abs(arr[i][1] - y);
-        if (dist < shortestPath) {
-            shortestPath = dist;
-            closestPoint = grid[arr[i][0]][arr[i][1]];
-        } else if (dist === shortestPath) {
-            closestPoint = ".";
-        }
+        totalDist += Math.abs(arr[i][0] - x) + Math.abs(arr[i][1] - y);
     }
 
-
-    // console.log(shortestPath + " to " + closestPoint);
-    return closestPoint;
+    return (totalDist < 10000);
 }
